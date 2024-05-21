9f5beb64f2de69d6de121aeb11d6b2c675b68756
deshashrikakade <119559216+deshashrikakade@users.noreply.github.com>
2024-05-21 15:11:52 +0530

    vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,43 @@
+#Create a VPC
+resource "aws_vpc" "myvpc"{
+    cidr_block = "10.0.0.0/16"
+    tags = {
+        Name = "MyVPC"
+    }
+}
+
+ #Create a public subnet
+resource "aws_subnet" "PublicSubnet"{
+    vpc_id = aws_vpc.myvpc.id
+    availability_zone = "ap-south-1a"
+    cidr_block = "10.0.1.0/24"
+}
+
+ #create a private subnet
+resource "aws_subnet" "PrivSubnet"{
+    vpc_id = aws_vpc.myvpc.id
+    cidr_block = "10.0.2.0/24"
+    map_public_ip_on_launch = true
+}
+
+
+ #create internet gateway
+resource "aws_internet_gateway" "myIgw"{
+    vpc_id = aws_vpc.myvpc.id
+}
+
+ #route tables for public subnet
+resource "aws_route_table" "PublicRT"{
+    vpc_id = aws_vpc.myvpc.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.myIgw.id
+    }
+}
+ 
+
+ #route table association public subnet 
+resource "aws_route_table_association" "PublicRTAssociation"{
+    subnet_id = aws_subnet.PublicSubnet.id
+    route_table_id = aws_route_table.PublicRT.id
+}
