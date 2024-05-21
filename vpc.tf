7d8c70d00bfe36e8970b412d1a101e93bd2bc9ff
deshashrikakade <119559216+deshashrikakade@users.noreply.github.com>
2024-05-21 15:14:59 +0530

    vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -9,7 +9,7 @@ resource "aws_vpc" "myvpc"{
  #Create a public subnet
 resource "aws_subnet" "PublicSubnet"{
     vpc_id = aws_vpc.myvpc.id
-    availability_zone = "ap-south-1a"
+    availability_zone = "us-east-1a"
     cidr_block = "10.0.1.0/24"
 }
 
